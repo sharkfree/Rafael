a28a6fc335b57b226c79532e68830626da2772e1
shark <vwildt@hotmail.de>
2025-10-31 13:39:09 +0100

    doca

--- Bope/src/src/Favelado.java
+++ Bope/src/src/Favelado.java
@@ -5,7 +5,7 @@ public class Favelado {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("favelado");
+		System.out.println("favelado doca");
 
 	}
 
